ec7a2eb42872ecef40221166456225e25a412e64
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-03-04 23:55:53 -0300

    add: seed data

--- cliente/tests.py
+++ cliente/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
 # Create your tests here.
+from cliente.models import ClCliente
+
+
+# ClCliente.objects.create(rut='12312312-3',nombres='Rosa Andrea',apellidos='Casas Cepeda',fecha_nacimiento='1990-01-10')
+# ClCliente.objects.create(rut='76924237-3', nombres='Rafaela Rogelio', apellidos='Rivero Arjona', fecha_nacimiento='1968-05-27')
+# ClCliente.objects.create(rut='62293877-9', nombres='Ariadna Amalia', apellidos='Quirós Mateos', fecha_nacimiento='1998-01-11')
+# ClCliente.objects.create(rut='36340551-1', nombres='Pepito Dalila', apellidos='Gil Vargas', fecha_nacimiento='1952-02-14')
+# ClCliente.objects.create(rut='73098925-6', nombres='Lope Enrique', apellidos='Bustamante Iglesia', fecha_nacimiento='2005-08-24')
+# ClCliente.objects.create(rut='65297627-0', nombres='Julieta Nélida', apellidos='Collado Menéndez', fecha_nacimiento='1946-08-10')
+# ClCliente.objects.create(rut='13642847-7', nombres='Luna Candelario', apellidos='Murillo Suarez', fecha_nacimiento='1946-11-14')
+# ClCliente.objects.create(rut='34726051-1', nombres='Paulino Toño', apellidos='Molina Fuente', fecha_nacimiento='1998-02-01')
+# ClCliente.objects.create(rut='31767488-4', nombres='Venceslás Rosenda', apellidos='Vázquez Juliá', fecha_nacimiento='1941-08-22')
+# ClCliente.objects.create(rut='76552137-5', nombres='Noa Zaida', apellidos='Alcalá Bueno', fecha_nacimiento='1987-06-12')
+# ClCliente.objects.create(rut='10130798-3', nombres='Leocadia Paloma', apellidos='Abella Uriarte', fecha_nacimiento='1978-07-04')
+# ClCliente.objects.create(rut='97393633-8', nombres='Jose Francisco Luna', apellidos='Morell Barros', fecha_nacimiento='1959-03-03')
+# ClCliente.objects.create(rut='19317948-9', nombres='Yéssica Clementina', apellidos='Girona Gómez', fecha_nacimiento='1952-07-23')
+# ClCliente.objects.create(rut='53533433-9', nombres='Nieves Candelaria', apellidos='Samper Bou', fecha_nacimiento='1944-02-11')
+# ClCliente.objects.create(rut='84262477-2', nombres='Miguel Sara', apellidos='Verdejo Tovar', fecha_nacimiento='1948-01-04')
+# ClCliente.objects.create(rut='11821855-1', nombres='Marcial Carmen', apellidos='Manuel Casanovas', fecha_nacimiento='1978-06-15')
+# ClCliente.objects.create(rut='71793040-8', nombres='Ofelia Ágata', apellidos='Frutos Maestre', fecha_nacimiento='1993-09-23')
+# ClCliente.objects.create(rut='71764500-8', nombres='Ciriaco Débora', apellidos='Plaza Molins', fecha_nacimiento='1966-02-12')
+# ClCliente.objects.create(rut='21901828-2', nombres='Luis Ángel Gerónimo', apellidos='Fajardo Estévez', fecha_nacimiento='1937-08-27')
+# ClCliente.objects.create(rut='35213863-6', nombres='Emperatriz Gala', apellidos='Juan Tamayo', fecha_nacimiento='1942-07-27')
+# ClCliente.objects.create(rut='76652281-5', nombres='Chuy Iris', apellidos='Castillo Conde', fecha_nacimiento='1938-06-04')
+# ClCliente.objects.create(rut='84999412-9', nombres='Purificación Cecilio', apellidos='Posada Zamorano', fecha_nacimiento='1970-09-05')
+# ClCliente.objects.create(rut='69406313-2', nombres='Azahara Octavia', apellidos='Gutiérrez Garrido', fecha_nacimiento='1990-04-01')
+# ClCliente.objects.create(rut='46366414-4', nombres='Cruz Leonardo', apellidos='Sanjuan Ugarte', fecha_nacimiento='1981-10-20')
+# ClCliente.objects.create(rut='14944831-1', nombres='Celestino Moreno', apellidos='Carlos Torrecilla', fecha_nacimiento='1978-10-18')
