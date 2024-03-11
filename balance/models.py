62e3cbf4e5ab75c9eccdbf05a7b52081d0332356
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-03-10 23:28:09 -0300

    add: new model ctainterna

--- balance/models.py
+++ balance/models.py
@@ -18,3 +18,14 @@ class BaRegistroPago(models.Model):
         verbose_name = 'Registro de Pago'
         verbose_name_plural = 'Registros de Pagos'
 
+
+class BaCtaInterna(models.Model):
+    nombre_cta = models.CharField(max_length=150)
+    monto_cta = models.IntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'ba_cta_interna' 
+        verbose_name = 'Cuenta Interna'
+        verbose_name_plural = 'Cuentas Internas'
